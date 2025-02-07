65929b5ceb211d00adf0a8631baa04fb834b42d7
Lucas Cancio <lucas.camargo@energisaterceiros.com.br>
2025-02-07 13:43:45 -0300

    ajuste em layout

--- src/pages/_layouts/public.tsx
+++ src/pages/_layouts/public.tsx
@@ -8,7 +8,7 @@ export function PublicLayout() {
         <Logo className="absolute top-0 size-40" />
         <Outlet />
       </div>
-      <div className="relative max-h-full flex justify-between rounded-lg border-foreground/5 bg-primary text-muted-foreground border-background m-8 box-border">
+      <div className="relative max-h-full flex justify-between rounded-lg border-foreground/5 bg-primary text-muted border-background m-8 box-border">
         <img src="/images/landing.svg" alt="Educação" className="flex-1" />
 
         <footer className="text-sm absolute bottom-8 left-1/2 transform -translate-x-1/2">
